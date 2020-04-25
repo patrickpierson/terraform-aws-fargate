1158d86a5766e332b5b58bb07e7c7a159d00591e
Patrick Pierson <patrick.c.pierson@gmail.com>
2020-04-25 14:23:21 -0400

    80 to 443 redirect

--- main.tf
+++ main.tf
@@ -338,6 +338,23 @@ resource "aws_lb_listener" "this" {
   }
 }
 
+resource "aws_lb_listener" "this80" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+  depends_on        = [aws_lb_listener.this]
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener_rule" "host_based_routing" {
   count = local.services_count > 0 ? local.services_count : 0
   listener_arn = aws_lb_listener.this.arn
